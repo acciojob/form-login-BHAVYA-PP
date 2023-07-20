0caa7f9a0391796cef8719d135c8e08f90e56c06
BHAVYA-PP <131146305+BHAVYA-PP@users.noreply.github.com>
2023-07-20 17:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,3 @@ let last =document.querySelector('#form1 input[name="lname"]');
 	let lname=last.value;
 	alert(fname+" "+lname);
 }
-	<form id="form1" onsubmit="getFormvalue()">
-		First name: <input type="text" name="fname"><br>
-		Last name: <input type="text" name="lname"><br>
-		<input type="submit" value="Submit">
-	</form>
-	
-    
