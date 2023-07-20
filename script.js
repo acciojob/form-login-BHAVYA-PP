b94b91baaf207125b28e8f924bc7d385ce95913c
BHAVYA-PP <131146305+BHAVYA-PP@users.noreply.github.com>
2023-07-20 17:38:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function getFormvalue() {
     //Write your code here
-
+let first =document.querySelector('#form1 input[name="fname"]');
+let last =document.querySelector('#form1 input[name="lname"]');
+	let fname=first.value;
+	let lname=last.value;
+	alert(fname+" "+lname);
 }
+	<form id="form1" onsubmit="getFormvalue()">
+		First name: <input type="text" name="fname"><br>
+		Last name: <input type="text" name="lname"><br>
+		<input type="submit" value="Submit">
+	</form>
+	
+    <script type="text/javascript" src="./script.js">
+</script>
+ 
+</body>
+ 
+</html>
+ 
